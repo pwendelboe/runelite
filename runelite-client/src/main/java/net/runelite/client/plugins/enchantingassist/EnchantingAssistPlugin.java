5a754ccdc71d9ce011eebe40575dbfade38a11cc
pwendelboe <pwendelboe.git@gmail.com>
2019-03-11 22:58:57 -0700

    trying to figure out what event to subscribe to

--- runelite-client/src/main/java/net/runelite/client/plugins/enchantingassist/EnchantingAssistPlugin.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/enchantingassist/EnchantingAssistPlugin.java
@@ -1,11 +1,21 @@
 package net.runelite.client.plugins.enchantingassist;
 
+//import com.google.common.eventbus.Subscribe;
+import net.runelite.client.eventbus.Subscribe;
 import com.google.inject.Provides;
 import javax.inject.Inject;
+import net.runelite.api.events.WidgetLoaded;
 import net.runelite.client.config.ConfigManager;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
 import net.runelite.client.ui.overlay.OverlayManager;
+import net.runelite.api.widgets.WidgetInfo;
+import net.runelite.api.events.ConfigChanged;
+import net.runelite.api.events.GameStateChanged;
+import net.runelite.api.events.VarbitChanged;
+import net.runelite.api.events.WidgetHiddenChanged;
+import net.runelite.api.events.WidgetLoaded;
+import net.runelite.api.widgets.Widget;
 
 @PluginDescriptor(
 	name = "Enchanting assist",
@@ -41,5 +51,28 @@ public class EnchantingAssistPlugin extends Plugin
 	{
 		overlayManager.remove(overlay);
 	}
+
+	@Subscribe
+	public void onWidgetHiddenChanged(WidgetHiddenChanged event)
+	{
+		//System.out.println(event);
+		//for (int i = 0; i < event.getWidget().getChildren().length;i++)
+		//{
+		//	System.out.println(event.getWidget().getChild(i).getText());
+		//}
+		//System.out.println(event.getWidget().getId());
+	}
+
+
+	@Subscribe
+	public void onWidgetLoaded(WidgetLoaded event)
+	{
+		//if (event.getGroupId())
+		System.out.println(event);
+		System.out.println(event.getGroupId());
+		System.out.println(event.getClass().getName());
+	}
+
+
 
 }
